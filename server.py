40acbead21bb1c4c20d69c1c1f8b5893b4182c8a
ShepLT1 <luke.t.shepherd@gmail.com>
2022-11-26 17:56:54 -0800

    Added ability to serve html templates

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello!</p>"
+    return render_template("index.html")
+
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
